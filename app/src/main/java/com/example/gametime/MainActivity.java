930fe3a79a7e442eab5903af986b7a37aa94d268
Jacob Smith <jacobellissmith@gmail.com>
2022-03-03 15:12:36 -0500

    test

--- app/src/main/java/com/example/gametime/MainActivity.java
+++ app/src/main/java/com/example/gametime/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements CreateAccountFrag
         }
     }
 
+    //testing
+
     @Override
     public void gotoCreateAccount() {
         getSupportFragmentManager()
