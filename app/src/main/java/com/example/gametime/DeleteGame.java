dbb9e30f6e475485860f23fcba7309a7d87c712f
Michael Shalapskiy <mshalaps@uncc.edu>
2022-03-23 00:08:53 -0700

    Added a function that deletes sub-collection(ChatRoom) and its sub-documents(Message-ID) when the host user deletes his game time event.

--- app/src/main/java/com/example/gametime/DeleteGame.java
+++ app/src/main/java/com/example/gametime/DeleteGame.java
@@ -3,8 +3,17 @@ package com.example.gametime;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.Serializable;
 
@@ -19,6 +28,7 @@ public class DeleteGame implements Serializable {
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        deleteAllMessagesWithinTheGameChatRoom(mGame, db);
                         db.collection("games").document(mGame.getGameId()).delete();
                     }
                 });
@@ -30,5 +40,32 @@ public class DeleteGame implements Serializable {
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    private void deleteAllMessagesWithinTheGameChatRoom(Game mGame, FirebaseFirestore db) {
+        db.collection("games").document(mGame.getGameId()).collection("ChatRoom")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(final QueryDocumentSnapshot doc : task.getResult()){
+                        if(doc.exists()){
+                            db.collection("games").document(mGame.getGameId())
+                                    .collection("ChatRoom").document(doc.getId())
+                                    .delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) { /* I will add text log here later on */ }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) { /* I will add text log here later on */}
+                            });
+                        }
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) { /* I will add text log here later on */ }
+        });
     }
 }
